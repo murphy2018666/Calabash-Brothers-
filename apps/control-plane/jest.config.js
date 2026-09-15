0139227799bd944e596aebf1a8ed39183c3e1fef
murphy2018666 <38279949+murphy2018666@users.noreply.github.com>
2026-09-15 16:40:30 +0800

    Add files via upload

---
+++ apps/control-plane/jest.config.js
@@ -0,0 +1,22 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('../../tsconfig.base.json');
+const { createJestPreset } = require('ts-jest');
+
+module.exports = {
+  ...createJestPreset(),
+  displayName: 'control-plane',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    }],
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!.*\\.mjs$)',
+  ],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/../../',
+  }),
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testMatch: ['**/*.spec.ts'],
+};
---
+++ apps/control-plane/jest.setup.ts
@@ -0,0 +1,4 @@
+/**
+ * Jest 全局设置：注册自定义匹配器
+ */
+require('./src/test-utils/matchers');
